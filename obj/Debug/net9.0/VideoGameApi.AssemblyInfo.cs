4cbcb41c6f0c8230b45c9b49ddc22e7a4c62a5d0
Moel <mohamedalkasaby1@gmail.com>
2025-08-31 20:53:14 +0300

    set up & get all & get by id

--- obj/Debug/net9.0/VideoGameApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/VideoGameApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee36a3352af257505833a7f350510bd7b5315169")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

ee36a3352af257505833a7f350510bd7b5315169
Moel <mohamedalkasaby1@gmail.com>
2025-08-31 20:50:46 +0300

    set up & get all & get by id

---
+++ Controllers/VideoGameController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        private List<Models.VideoGame> videoGames = new List<Models.VideoGame>
+        {
+            new Models.VideoGame { id = 1, Title = "The Legend of Zelda: Breath of the Wild", Platform = "Nintendo Switch", Developer = "Nintendo", Publisher = "Nintendo" },
+            new Models.VideoGame { id = 2, Title = "God of War", Platform = "PlayStation 4", Developer = "Santa Monica Studio", Publisher = "Sony Interactive Entertainment" },
+            new Models.VideoGame { id = 3, Title = "Red Dead Redemption 2", Platform = "PlayStation 4, Xbox One, PC", Developer = "Rockstar Games", Publisher = "Rockstar Games" }
+        };
+
+        [HttpGet]
+
+        public ActionResult<List<Models.VideoGame>> Get()
+        {
+            return Ok(videoGames);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Models.VideoGame> Get_VideoGame(int id)
+        {
+            var videoGame = videoGames.FirstOrDefault(game => game.id == id);
+            if (videoGame is null)
+                return NotFound();
+            return Ok(videoGame);
+        }
+    }
+}
+
---
+++ Models/VideoGame.cs
@@ -0,0 +1,11 @@
+﻿namespace VideoGameApi.Models
+{
+    public class VideoGame
+    {
+        public int id{ get; set; }
+        public string? Title { get; set; }
+        public string? Platform { get; set; }
+        public string? Developer { get; set; }
+        public string? Publisher { get; set; }
+    }
+}
